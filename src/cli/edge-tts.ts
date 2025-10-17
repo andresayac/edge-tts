62333cdc01a5176ed5c879bc7a8a778dc7829dcf
andresayac <andresayac@gmail.com>
2025-10-17 11:10:56 -0500

    chore: bump version to 1.4.4 in package.json and edge-tts.ts

--- src/cli/edge-tts.ts
+++ src/cli/edge-tts.ts
@@ -7,7 +7,7 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .version('1.4.2')
+  .version('1.4.4')
   .addCommand(SynthesizeCommand)
   .addCommand(VoiceListCommand);
 
